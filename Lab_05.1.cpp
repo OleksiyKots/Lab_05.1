1f3bd8f02f66e1fb257c1bc06544cb8b1c23196e
unknown <oleksiykots10@gmail.com>
2024-10-02 15:31:13 +0300

    first commit

---
+++ Lab_05.1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+double h(const double x, const double y); // прототип
+int main()
+{
+	double s, t;
+	cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+	double d = (h(s, t * t) + h(t, 1 + pow (s, 2))) / (1 + h(s*t, 1));
+	cout << "d = " << d << endl;
+	return 0;
+}
+double h(const double x, const double y) // визначення
+{
+	return (pow(x, 2) + sin(x*y) + pow(y, 2)) /  (1 + pow(x, 2) + pow(y, 2));
+}
